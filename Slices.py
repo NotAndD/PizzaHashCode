190f8d818c27745522d0d0df3f06d8ace94e5c57
Andrea Dolfi <andrea.dolfi0@gmail.com>
2017-02-17 22:34:02 +0100

    adding basic support for slices logic

---
+++ Slices.py
@@ -0,0 +1,15 @@
+
+class Slice:
+    def __init__(self, theMatrix, x1, y1):
+        self.theMatrix = theMatrix
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x1
+        self.y2 = y1
+        self.numOfCells = 1
+
+    def addCells(self, x, y):
+        pass
+
+    def countIngredient(self, inredient):
+        pass

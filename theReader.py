f9f2ef24d50cf6102445739b5a790851ca4956af
Andrea Dolfi <andrea.dolfi0@gmail.com>
2017-02-17 22:03:54 +0100

    adding reader of input config

---
+++ theReader.py
@@ -0,0 +1,21 @@
+from PizzaMatrix import PizzaMatrix
+
+class TheReader:
+
+    def __init__(self, inputFileName):
+        self.theMatrix = None
+        self.fileName = inputFileName
+        self.numberOfRows = 0
+        self.numberOfColumns = 0
+        self.minNumberOfIngredients = 0
+        self.maxCellsInSlice = 0
+
+    def readConfiguration(self):
+        theFile = open(self.fileName)
+        firstLine = theFile.readline().split(' ')
+        self.numberOfRows = firstLine[0]
+        self.numberOfColumns = firstLine[1]
+        self.minNumberOfIngredients = firstLine[2]
+        self.maxCellsInSlice = firstLine[3]
+        lines = theFile.readlines()
+        self.theMatrix = PizzaMatrix(lines)

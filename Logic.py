ac4e50ecceb82976f8522f51663ad9ba81c65062
Mikedev <mdv1994@gmail.com>
2017-02-17 22:43:46 +0100

    Attempt to write search

--- Logic.py
+++ Logic.py
@@ -1,7 +1,5 @@
 from functools import reduce
-
 from theReader import TheReader
-
 
 class Brain:
     def __init__(self, configFile):
@@ -11,3 +9,25 @@ class Brain:
     def searchMinOccurencesOfIngredient(self):
         numResult = reduce(lambda a,b : a+b, map(lambda pizzaRow : map(lambda cell:1 if(cell.type == 'M') else -1), self.reader.theMatrix.matrix))
         return 'M' if numResult > 0 else 'T'
+
+    def search(self):
+        minIngredient = self.searchMinOccurencesOfIngredient()
+        pizzaMatrix = self.reader.theMatrix.matrix
+        startingPositions = self.getStartingPositions(pizzaMatrix)
+
+
+
+    def getStartingPositions(self, pizzaMatrix):
+        startingPositions = []
+        for i in range(pizzaMatrix):
+            for j in range(pizzaMatrix):
+                if not pizzaMatrix[i][j].explored and pizzaMatrix[i][j].type == minIngredient:
+                    startingPositions.append((i,j))
+        return startingPositions
+
+
+
+
+
+
+
